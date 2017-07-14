2c4cfdea70862064a97d919b13dbac0ac056d683
ChrisJXS <jiaxians@andrew.cmu.edu>
2017-07-14 11:39:54 -0400

    basic functionalities of the boxes work

--- src/index.js
+++ src/index.js
@@ -1,8 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+var injectTapEventPlugin = require("react-tap-event-plugin");
+injectTapEventPlugin();
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+ReactDOM.render(
+  <App />,
+  document.getElementById('root')
+);
